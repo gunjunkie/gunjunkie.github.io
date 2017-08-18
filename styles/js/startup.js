c926e3080325bf6503a9ead077b6c858444616e1
junok <u0830932@utah.edu>
2017-08-18 14:08:02 -0600

    button done

--- styles/js/startup.js
+++ styles/js/startup.js
@@ -1,6 +1,8 @@
 var slideIndex = 0;
 var timer = null;
 // showSlides(); autosliding.
+var lbtn = document.getElementById('leftBtn');
+var rbtn = document.getElementById('rgtBtn');
 
 function showSlides() {
     var i;
@@ -25,4 +27,25 @@ function carouselNext(){
     slides[slideIndex-1].style.display = "inline-block";
     clearTimeout(timer);
     // timer = setTimeout(showSlides, 6000);
+}
+
+function carouselBack(){
+    var i;
+    var slides = document.getElementsByClassName("ImageView");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex--;
+    if (slideIndex < 1) {slideIndex = 3}
+    slides[slideIndex-1].style.display = "inline-block";
+    clearTimeout(timer);
+    // timer = setTimeout(showSlides, 6000);
+}
+
+rbtn.onclick = function(){
+    carouselNext();
+}
+
+lbtn.onclick = function(){
+    carouselBack();
 }
